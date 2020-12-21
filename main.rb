765b1132b804426b885deb48b7347ce97f3da24d
Boon Kit <gbk1386@gmail.com>
2020-12-21 03:55:12 +0000

    Add comments

--- main.rb
+++ main.rb
@@ -3,9 +3,13 @@
 # the next loop will decrease the size by 1, so the size is n - i - 1
 
 def bubble_sort(array)
+  # big loop
   for i in 0...array.length-1 do
+    # length for the current passes
     for j in 0...array.length-i-1 do
+      # compare current value and next value
       if array[j] >= array[j+1]
+        # if true, swap the value
         array[j], array[j+1] = array[j+1], array[j]
       end
     end
