ce2e2b4aaed390d3eae8627d854c3623e9d40eff
Boon Kit <gbk1386@gmail.com>
2020-12-21 04:17:40 +0000

    Refactor the loop

--- main.rb
+++ main.rb
@@ -4,15 +4,17 @@
 
 def bubble_sort(array)
   # big loop
-  for i in 0...array.length-1 do
+  array.length.times do
     # length for the current passes
-    for j in 0...array.length-i-1 do
+    i = 1
+    for j in 0...array.length-i do
       # compare current value and next value
       if array[j] >= array[j+1]
         # if true, swap the value
         array[j], array[j+1] = array[j+1], array[j]
       end
     end
+    i += 1
   end
   array
 end
